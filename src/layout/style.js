cbb5a583a67187f6a28b67d592a67882a74af69c
chenyc <chenyc@gzsendi.cn>
2025-02-08 23:39:58 +0800

    style: 优化容器样式

--- src/layout/style.js
+++ src/layout/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const LayoutStyle = styled.div`
-	height: 100%;
+	min-height: 100%;
     display: flex;
     flex-direction: column;
     background: #f2f3f5;
