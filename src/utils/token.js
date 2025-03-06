b289ea2fe92a0815b02e188de6b12160440e80c6
chenyc <chenyc@gzsendi.cn>
2025-03-06 16:14:06 +0800

    style: 格式化代码

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,13 +1,13 @@
-const tokenKey = 'cyc_token'
+const tokenKey = 'cyc_token';
 
 export const setToken = (token) => {
-  localStorage.setItem(tokenKey, token)
-}
+	localStorage.setItem(tokenKey, token);
+};
 
 export const getToken = () => {
-  return localStorage.getItem(tokenKey)
-}
+	return localStorage.getItem(tokenKey);
+};
 
 export const removeToken = () => {
-  localStorage.removeItem(tokenKey)
-}
+	localStorage.removeItem(tokenKey);
+};
